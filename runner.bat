1bbc1289b560d327845071318f5a40154e4bbaf9
ismayeel gdr <ismayeelanver@github.com>
2024-04-18 13:29:56 +0530

    requirements

--- runner.bat
+++ runner.bat
@@ -1,3 +1,5 @@
 @echo off
-
+echo Installing requirements
+pip install -r requirements\ismayeel-requirements.txt > nul
+echo Launching
 python Red_and_white.py > nul
--- runner.sh
+++ runner.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+echo "Installing requirements"
+
+pip install -r ./requirements/ismayeel-requirements.txt > /dev/null
+
+echo "Launching"
+
 python ./Red_and_white.py > /dev/null
